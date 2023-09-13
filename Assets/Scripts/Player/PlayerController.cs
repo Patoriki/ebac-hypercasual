fc6fabe47f2abadcba3edadd6de79088772b816e
Patoriki <142239657+Patoriki@users.noreply.github.com>
2023-09-13 07:24:57 -0300

    add Enemy and Lerp adjustment

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Lerp")]
+    public Transform target;
+    public float lerpSpeed;
+
+    [Space]
     public float speed;
+
+    public string tagToCheckEnemy = "Enemy";
+
+    private bool _canRun;
+    private Vector3 _pos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _canRun = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!_canRun) return;
+
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == tagToCheckEnemy)
+        {
+            _canRun = false;
+        }
     }
 }
